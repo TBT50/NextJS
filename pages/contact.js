e20d05f84cd0744eb7b812d1276b9f647570965a
Piotrek <piotrek380@gmail.com>
2021-03-20 14:21:11 +0100

    Contact page - add random header and navigation

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export default function Contact() {
   return (
     <div className="container">
       <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Homepage</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/posts/first-post">
+                <a>First Post</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
         <h1>This is the contact page</h1>
       </header>
     </div>
